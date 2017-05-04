ec8d1f4b723a4e9df9e90abf677e3aa8a67edbff
jbakog <jbako@cosmo-one.gr>
2017-05-04 14:08:28 +0300

    migrate angularjs2angular initial commit

---
+++ app/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var static_1 = require("@angular/upgrade/static");
+var app_module_1 = require("./app.module");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule).then(function (platformRef) {
+    var upgrade = platformRef.injector.get(static_1.UpgradeModule);
+    upgrade.bootstrap(document.documentElement, ['phonecatApp']);
+});
+//# sourceMappingURL=main.js.map
